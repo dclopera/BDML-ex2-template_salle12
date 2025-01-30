44bdb484847ee6980ec4f340e0b4d50c43af44a8
lialvareza1 <lm.alvareza1@uniandes.edu.co>
2025-01-29 19:26:39 -0500

    Update model2.R

--- model2.R
+++ model2.R
@@ -1,2 +1,26 @@
 
-# Fill here the code to estimate the linear_model2 from the notebook
+## leverage 
+db_int<- db_int %>% mutate(leverage = hatvalues(linear_model))
+
+
+## residuals
+db_int<- db_int %>% mutate(residuals= linear_model$residuals)
+N <- nrow(db_int)
+
+p <- mean(db_int$leverage)
+p
+
+cutt <- 3*p
+cutt
+
+db_int2 <-  db_int %>% 
+  dplyr:: filter(leverage<= cutt)
+# re run the model
+linear_model2<- lm(totalHoursWorked ~ ofic_ingLab + nmenores  +  nmenores*gender + H_Head + age + gender, data=db_int2  )
+
+
+stargazer(linear_model, linear_model2, type="text",
+          covariate.labels=c("Mean Ocu Income","N under 18","Male",
+                             "Hausehold Head","Age", "N under 18 x Male" ))
+
+
